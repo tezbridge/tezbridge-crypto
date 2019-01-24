f7f6593dcaf3fc08937b4b50651c148d46d8d55f
catsigma <im@catsigma.com>
2019-01-24 22:24:18 +0800

    add localop

--- src/PsddFKi3/localop.js
+++ src/PsddFKi3/localop.js
@@ -2,19 +2,33 @@
 
 import codec from './codec'
 
+function tagEncode(input : Uint8Array, prefix : string) {
+	const tag_mapping = {
+		ed25519_public_key_hash: '01',
+		secp256k1_public_key_hash: '02',
+		p256_public_key_hash: '03'
+	}
+
+	if (!tag_mapping[prefix])
+		throw `Prefix: ${prefix} not found`
+
+	return tag_mapping[prefix] + codec.toHex(input)
+}
+
 const op_hex_mapping = {
-	transaction() {
+	transaction(op : Object) {
+		const result = [80]
+		
+	},
+	origination(op : Object) {
 
 	},
-	origination() {
-
-	},
-	reveal() {
+	reveal(op : Object) {
 
 	}
 }
 
-export function forgeOperation(branch: string, contents: Array<Object>) {
+export function forgeOperation(branch : string, contents : Array<Object>) {
 	const branch_bytes = codec.bs58checkDecode(branch)
 	const result = [codec.toHex(branch_bytes)]
 
