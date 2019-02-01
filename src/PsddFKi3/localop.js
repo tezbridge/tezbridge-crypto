ade824602b51840088c30005f17b4caf250beab9
catsigma <im@catsigma.com>
2019-02-01 15:59:35 +0800

    using toTzBytes in forgeOperation

--- src/PsddFKi3/localop.js
+++ src/PsddFKi3/localop.js
@@ -2,44 +2,19 @@
 
 import codec from './codec'
 
-const tag_mapping = {
-  ed25519_public_key_hash: '00',
-  secp256k1_public_key_hash: '01',
-  p256_public_key_hash: '02',
-
-  ed25519_public_key: '00',
-  secp256k1_public_key: '01',
-  p256_public_key: '02'
-}
-
-function pushContractId(arr : Array<string>, prefix, bytes) {
-  if (prefix.name === 'contract_hash') {
-    arr.push('01')
-    arr.push(codec.toHex(bytes))
-    arr.push('00')
-  } else {
-    arr.push('00')
-    arr.push(tag_mapping[prefix.name])
-    arr.push(codec.toHex(bytes))
-  }
-}
 
 const op_hex_mapping = {
   transaction(op : Object) {
     const result = ['08']
-    const source_prefix = codec.bs58checkPrefixPick(op.source)
-    const dest_prefix = codec.bs58checkPrefixPick(op.destination)
-    const source_bytes = codec.bs58checkDecode(op.source, source_prefix.bytes)
-    const dest_bytes = codec.bs58checkDecode(op.destination, dest_prefix.bytes)
   
-    pushContractId(result, source_prefix, source_bytes)
+    result.push(codec.toTzBytes(op.source))
 
     ;[op.fee, op.counter, op.gas_limit, op.storage_limit, op.amount].forEach(x => {
       const hex = codec.encodeZarithUInt(x)
       result.push(hex)
     })
 
-    pushContractId(result, dest_prefix, dest_bytes)
+    result.push(codec.toTzBytes(op.destination))
 
     result.push(op.parameters ? 'FF' : '00')
     if (op.parameters) {
@@ -52,20 +27,15 @@ const op_hex_mapping = {
   },
   origination(op : Object) {
     const result = ['09']
-    const source_prefix = codec.bs58checkPrefixPick(op.source)
-    const source_bytes = codec.bs58checkDecode(op.source, source_prefix.bytes)
-    const managerpkh_prefix = codec.bs58checkPrefixPick(op.managerPubkey)
-    const managerpkh_bytes = codec.bs58checkDecode(op.managerPubkey, managerpkh_prefix.bytes)
 
-    pushContractId(result, source_prefix, source_bytes)
+    result.push(codec.toTzBytes(op.source))
 
     ;[op.fee, op.counter, op.gas_limit, op.storage_limit].forEach(x => {
       const hex = codec.encodeZarithUInt(x)
       result.push(hex)
     })
 
-    result.push(tag_mapping[managerpkh_prefix.name])
-    result.push(codec.toHex(managerpkh_bytes))
+    result.push(codec.toTzBytes(op.managerPubkey, true))
 
     result.push(codec.encodeZarithUInt(op.balance))
 
@@ -73,11 +43,7 @@ const op_hex_mapping = {
     result.push(op.delegatable ? 'FF' : '00')
     result.push(op.delegate ? 'FF' : '00')
     if (op.delegate) {
-      const delegate_prefix = codec.bs58checkPrefixPick(op.delegate)
-      const delegate_bytes = codec.bs58checkDecode(op.delegate, delegate_prefix.bytes)
-
-      result.push(tag_mapping[delegate_prefix.name])
-      result.push(codec.toHex(delegate_bytes))
+      result.push(codec.toTzBytes(op.delegate, true))
     }
 
     result.push(op.script ? 'FF' : '00')
@@ -95,26 +61,21 @@ const op_hex_mapping = {
   },
   reveal(op : Object) {
     const result = ['07']
-    const source_prefix = codec.bs58checkPrefixPick(op.source)
-    const source_bytes = codec.bs58checkDecode(op.source, source_prefix.bytes)
-    const pk_prefix = codec.bs58checkPrefixPick(op.public_key)
-    const pk_bytes = codec.bs58checkDecode(op.public_key, pk_prefix.bytes)
 
-    pushContractId(result, source_prefix, source_bytes)
+    result.push(codec.toTzBytes(op.source))
 
     ;[op.fee, op.counter, op.gas_limit, op.storage_limit].forEach(x => {
       const hex = codec.encodeZarithUInt(x)
       result.push(hex)
     })
 
-    result.push(tag_mapping[pk_prefix.name])
-    result.push(codec.toHex(pk_bytes))
+    result.push(codec.toTzBytes(op.public_key, true))
 
     return result.join('')
   }
 }
 
-export function forgeOperation(branch : string, contents : Array<Object>) {
+export function forgeOperation(contents : Array<Object>, branch : string) {
   const branch_bytes = codec.bs58checkDecode(branch)
   const result = [codec.toHex(branch_bytes)]
 
